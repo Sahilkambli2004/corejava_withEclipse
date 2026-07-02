f8203674a6c391c466126c990c7bf175dc6c36c9
Sahilkambli2004 <gdark7823@gmail.com>
2026-07-02 13:01:34 +0530

    commit

--- Java_Project/src/basics/Prac1.java
+++ Java_Project/src/basics/Prac1.java
@@ -1,10 +1,15 @@
 package basics;
 
 public class Prac1 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //predefined 
+		
+		System.out.println("Core Java");
+		
+		System.out.println();
+		
+		
 	}
 
 }
